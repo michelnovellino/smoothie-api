66451c4a619d090380c0745a859f53e2c18d6eae
michel novellino <michelnovellino.programador@gmail.com>
2020-05-15 20:31:40 -0400

    connect and add cors

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
+var mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 const Smoothies = require('./routes/smoothies');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,4 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/smoothies', Smoothies);
+mongoose.connect('mongodb://localhost/smoothies-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 module.exports = app;
