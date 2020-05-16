28b8c2ee08436b4c5c5ebeb13c6ab1598ac42685
michel novellino <michelnovellino.programador@gmail.com>
2020-05-16 14:22:11 -0400

    add dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -6,7 +7,7 @@ const cors = require('cors');
 var mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
 const Smoothies = require('./routes/smoothies');
-
+const DB = process.env.DB;
 var app = express();
 app.use(cors());
 app.use(logger('dev'));
@@ -17,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/smoothies', Smoothies);
-mongoose.connect('mongodb://localhost/smoothies-app', {
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
